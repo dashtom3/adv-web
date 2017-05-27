c6876a448531b4c5c425b7032e378eb5bbdc5a5f
戴费立 <lovedharni@sina.com>
2017-05-27 18:21:30 +0800

    我的广告页面

--- adv_admin/src/views/nav2/myAdv.vue
+++ adv_admin/src/views/nav2/myAdv.vue
@@ -8,42 +8,100 @@
         </el-form-item>
       </el-form>
     </el-col>
+    <div class="clearfix"></div>
+    <div class="pop_up">
+      <div class="wait_word">弹出广告(仅能存一条)</div>
+    </div>
+    <el-table :data="tablePop" border style="width: 100%">
+      <el-table-column prop="advid" label="广告id">
+      </el-table-column>
+      <el-table-column prop="name" label="名称">
+      </el-table-column>
+      <el-table-column prop="des" label="描述">
+      </el-table-column>
+      <el-table-column prop="business" label="所属商铺">
+      </el-table-column>
+      <el-table-column prop="Aorder" label="是否下单">
+      </el-table-column>
+      <el-table-column prop="file" label="文件">
+      </el-table-column>
+      <el-table-column prop="time" label="时长">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template scope="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+    <div class="pop_up">
+      <div class="wait_word">滚动广告</div>
+    </div>
+    <el-table :data="tableData" border style="width: 100%">
+      <el-table-column prop="advid" label="广告id">
+      </el-table-column>
+      <el-table-column prop="name" label="名称">
+      </el-table-column>
+      <el-table-column prop="des" label="描述">
+      </el-table-column>
+      <el-table-column prop="business" label="所属商铺">
+      </el-table-column>
+      <el-table-column prop="Aorder" label="是否下单">
+      </el-table-column>
+      <el-table-column prop="file" label="文件">
+      </el-table-column>
+      <el-table-column prop="time" label="时长">
+      </el-table-column>
+      <el-table-column label="操作">
+        <template scope="scope">
+          <el-button size="small" @click="handleEdit(scope.$index, scope.row)">编辑</el-button>
+          <el-button size="small" type="danger" @click="handleDelete(scope.$index, scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
     <!-- 弹出窗开始 -->
     <el-dialog title="添加广告" :visible.sync="dialogFormVisible">
       <el-form :model="form">
         <el-form-item label="商铺类型" :label-width="formLabelWidth">
-          <el-select v-model="form.type" placeholder="请选择商铺类型">
+          <el-select v-model="type" placeholder="请选择商铺类型">
             <el-option label="自家商铺广告" value="my"></el-option>
             <el-option label="其他店铺广告" value="other"></el-option>
           </el-select>
         </el-form-item>
-        <el-form-item label="名称" :label-width="formLabelWidth">
-          <el-input v-model="form.name" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="描述" :label-width="formLabelWidth">
-          <el-input v-model="form.des" auto-complete="off"></el-input>
-        </el-form-item>
-        <el-form-item label="广告类型" :label-width="formLabelWidth">
-          <el-select v-model="form.adtype" placeholder="请选择广告类型">
-            <el-option label="滚动广告" value="scroll"></el-option>
-            <el-option label="弹出广告" value="tanchu"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="是否下单" :label-width="formLabelWidth">
-          <el-select v-model="form.list" placeholder="请选择">
-            <el-option label="是" value="scroll"></el-option>
-            <el-option label="否" value="tanchu"></el-option>
-          </el-select>
-        </el-form-item>
-        <el-form-item label="文件" :label-width="formLabelWidth">
-        <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList">
-          <el-button size="small" type="primary">点击上传</el-button>
-          <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
-        </el-upload>
-        </el-form-item>
-        <el-form-item label="时长(仅上传文件时图片时显示)" :label-width="formLabelWidth">
-          <el-input v-model="form.time" auto-complete="off"></el-input>
-        </el-form-item>
+        <div v-show="other">
+          <el-form-item label="广告id" :label-width="formLabelWidth">
+            <el-input v-model="form.advidd" auto-complete="off"></el-input>
+          </el-form-item>
+        </div>
+        <div v-show="my">
+          <el-form-item label="名称" :label-width="formLabelWidth">
+            <el-input v-model="form.name" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="描述" :label-width="formLabelWidth">
+            <el-input v-model="form.des" auto-complete="off"></el-input>
+          </el-form-item>
+          <el-form-item label="广告类型" :label-width="formLabelWidth">
+            <el-select v-model="form.adtype" placeholder="请选择广告类型">
+              <el-option label="滚动广告" value="scroll"></el-option>
+              <el-option label="弹出广告" value="tanchu"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="是否下单" :label-width="formLabelWidth">
+            <el-select v-model="form.list" placeholder="请选择">
+              <el-option label="是" value="scroll"></el-option>
+              <el-option label="否" value="tanchu"></el-option>
+            </el-select>
+          </el-form-item>
+          <el-form-item label="文件" :label-width="formLabelWidth">
+            <el-upload class="upload-demo" action="https://jsonplaceholder.typicode.com/posts/" :on-preview="handlePreview" :on-remove="handleRemove" :file-list="fileList">
+              <el-button size="small" type="primary">点击上传</el-button>
+              <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+            </el-upload>
+          </el-form-item>
+          <el-form-item label="时长(仅上传文件时图片时显示)" :label-width="formLabelWidth">
+            <el-input v-model="form.time" auto-complete="off"></el-input>
+          </el-form-item>
+        </div>
       </el-form>
       <div slot="footer" class="dialog-footer">
         <el-button @click="dialogFormVisible = false">取 消</el-button>
@@ -51,28 +109,6 @@
       </div>
     </el-dialog>
     <!-- 弹出窗结束 -->
-    <el-table :data="tableData" border style="margin-top: 20px; width: 100%">
-      <el-table-column prop="advid" label="广告id" width="150">
-      </el-table-column>
-      <el-table-column prop="name" label="名称" width="150">
-      </el-table-column>
-      <el-table-column prop="des" label="描述" width="180">
-      </el-table-column>
-      <el-table-column prop="business" label="所属商铺" width="150">
-      </el-table-column>
-      <el-table-column prop="Aorder" label="是否下单" width="120">
-      </el-table-column>
-      <el-table-column prop="file" label="文件" width="150">
-      </el-table-column>
-      <el-table-column prop="time" label="时长" width="150">
-      </el-table-column>
-      <el-table-column label="操作" width="150">
-        <template scope="scope">
-          <el-button @click="handleClick" type="text" size="small">查看</el-button>
-          <el-button type="text" size="small">编辑</el-button>
-        </template>
-      </el-table-column>
-    </el-table>
   </div>
 </template>
 
@@ -80,6 +116,8 @@
   export default {
     data() {
       return {
+        my: true,
+        other: false,
         dialogFormVisible: false,
         formLabelWidth: '120px',
         tableData: [],
@@ -91,6 +129,7 @@
           list: '',
           time: '',
         },
+        type: '',
         fileList: [],
         tableData: [{
           advid: '123',
@@ -118,7 +157,32 @@
           Aorder: '是',
           file: 'hahahah',
           time: '20s',
-        }]
+        }],
+        tablePop: [{
+          advid: '123',
+          name: '衣服优惠',
+          des: '无',
+          business: '小酱软件',
+          Aorder: '是',
+          file: 'hahahah',
+          time: '20s',
+        }],
+      }
+    },
+    watch: {
+      type: function() {
+        var that = this;
+        //console.log(that.type,'22');
+        if (that.type == 'other') {
+          that.my = false;
+          that.other = true;
+          return false
+        }
+        if (that.type == 'my') {
+          that.my = true;
+          that.other = false;
+          return false;
+        }
       }
     },
     methods: {
@@ -142,5 +206,17 @@
 <style>
   .el-dialog__header {
     text-align: center;
+  }
+  .pop_up {
+    width: 100%;
+    height: 50px;
+    line-height: 50px;
+    font-size: 18px;
+    background-color: #eaeaea;
+    margin-top: 20px;
+    font-size: 15px;
+  }
+  .wait_word {
+    margin-left: 20px;
   }
 </style>
